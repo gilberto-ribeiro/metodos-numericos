f63f0e6943352721a4a700723338b8cf6778665c
Gilberto Ribeiro Pinto Júnior <gilbertoribeirocvt@gmail.com>
2024-08-21 21:50:39 +0000

    Exercícios 2 e 3 sobre zero de função

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use core::f64;
+
 use metodos_numericos::*;
 
 fn main() {
     // let intervalo: (f64, f64) = (300.0, 500.0);
-    let tolerancia: f64 = 1e-10;
-    let resultado = zero_de_funcao::secante(exercicio_1, (400.0, 450.0), tolerancia);
+    let tolerancia: f64 = 1e-12;
+    let resultado = zero_de_funcao::secante(exercicio_3, (0.0, 1.0), tolerancia);
     println!("Raiz: {}", resultado.raiz);
     println!("Iterações: {}", resultado.iteracoes);
     println!("Erro: {}", resultado.erro);
@@ -26,4 +28,28 @@ fn exercicio_1(x: f64) -> f64 {
             + a[3] * temperatura_1.powi(3) / 3.0
             + 0.25 * a[4] * temperatura_1.powi(4))
         - (pressao_2 / pressao_1).ln()
+}
+
+fn exercicio_2(x: f64) -> f64 {
+    let q: f64 = x;
+    let D: f64 = 25.4e-3;
+    let v: f64 = (4.0 / 1000.0) * (q / (f64::consts::PI * D * D));
+    let epsilon: f64 = 0.0457e-3;
+    let delta_p: f64 = -2e5;
+    let rho: f64 = 1000.0;
+    let mu: f64 = 1e-3;
+    let L: f64 = 50.0;
+    let Re: f64 = rho * v * D / mu;
+    let f: f64 = (-4.0 * (0.27 * epsilon / D + (7.0 / Re).powf(0.9)).log10()).powi(-2);
+    let l_wf: f64 = 2.0 * f * L * v * v / D;
+    delta_p / rho + l_wf
+}
+
+fn exercicio_3(x: f64) -> f64 {
+    let z_l: f64 = x;
+    let temp: f64 = 100.0;
+    let temp_a: f64 = 25.0;
+    let temp_w: f64 = 500.0;
+    let n: f64 = 3.5;
+    (temp - temp_a) / (temp_w - temp_a) - ((n * (1.0 - z_l)).cosh() / (n).cosh())
 }
